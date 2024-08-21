fc733887ed2d390e632283451947db5b2d8ef7f4
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-21 10:51:59 +0900

    JSON server 연동하여 데이터 가져오기

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src="https://lp2.hm.com/hmgoepprod?set=source[/ac/45/ac45ec01138f7d27cc8990e0dfb5cce7eb2fc424.jpg],origin[dam],category[ladies_jacketscoats_coats],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]"/>
+        <div>Consciout Choice</div>
+        <div>벨티드 트웰코트</div>
+        <div>99000</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/page/ProductAllPage.js
+++ src/page/ProductAllPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAllPage = () => {
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`
+    let response = await fetch(url)
+    let data = await response.json();
+    console.log(data);
+    setProductList(data);
+  }
+
+
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+
+
   return (
     <div>
-      
+
       <h1>전체 상품 페이지</h1>
-      
+      <ProductCard/>
     </div>
   )
 }

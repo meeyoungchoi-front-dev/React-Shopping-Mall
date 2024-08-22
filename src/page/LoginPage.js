94e8e27c7f692b5b4298268a0d0eefb725c58878
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-22 11:01:19 +0900

    로그인 안했을시 로그인 페이지로, 로그인 성공시 메인페이지로 navigate

--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -1,21 +1,41 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import { useNavigate } from 'react-router-dom';
 
-const LoginPage = () => {
+const LoginPage = ({setAuthenticate}) => {
+  const navigate = useNavigate();
+  const loginUser = (event) => {
+    event.preventDefault();
+    setAuthenticate(true);
+    navigate("/");
+  }
+
   return (
-    <div>
+    <Container>
       <h1>로그인</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form onSubmit={(event) => {loginUser(event)}}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="이메일을 입력하세요" />
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            이메일 주소를 입력하세요
+          </Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="비밀번호를 입력하세요" />
+          <Form.Control type="password" placeholder="Password" />
         </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit" >
+          로그인
+        </Button>
       </Form>
-    </div>
+    </Container>
   )
 }
 
--- src/page/ProductDetailPage.js
+++ src/page/ProductDetailPage.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-const ProductDetailPage = () => {
+const ProductDetailPage =  () => {
+  let {id} = useParams();
+  const [product, setProduct] = useState(null);
+  const getProductDetail = async () => {
+    let url = `http://localhost:5000/products/${id}`
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProduct(data);
+  }
+  useEffect(() => {
+    getProductDetail()
+  }, []);
+
+
+
   return (
     <div>
-      <h1>상품 상세 페이지</h1>
+      <Container>
+        <Row>
+          <Col className="product-img">
+              <img src={product?.img}/>
+          </Col>
+          <Col>
+            <div>{product?.title}</div>
+            <div>{product?.price}</div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
